6132828fc0c37f4a3c08bd7fa81a1a758677a85b
manticorevault <artur_seilah@hotmail.com>
2021-01-13 12:33:16 +0000

    Created the production dockerfiles, updated the Tests and the other pages for deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from "react"
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import otherpage from "./otherpage"
-import Fib from "./Fib"
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 
 class App extends Component {
   render() {
@@ -13,18 +12,16 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-
-            <Link to="/"> Home </Link>
-            <Link to="/otherpage"> Other Page </Link>
-
-            <div>
-              <Route exact path="/" Component={Fib} />
-              <Route path="otherpage" Component={otherpage} />
-            </div>
+            <h1 className="App-title">Fib Calculator UPDATED!</h1>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
           </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
         </div>
       </Router>
-
     );
   }
 }
